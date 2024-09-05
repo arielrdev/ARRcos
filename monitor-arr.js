7c71901475665e6c828a8c5de2684cb047c943b3
Eder <eder.arodriguez1999@gmail.com>
2024-09-04 22:17:48 -0600

    monitor-arr.js: Actualizado

--- monitor-arr.js
+++ monitor-arr.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import notifier from 'node-notifier';
 import schedule from 'node-schedule';
-import open from 'open';
+
 /** URLs a Monitorear */
 const urls = [
     'https://www.bienlinea.bi.com.gt/test.asp',
@@ -9,43 +9,44 @@ const urls = [
     'https://crm.bi.com.gt/test.asp'
 ]
 
+const notificacionMap = new Map();
+
 /** Funcion para verificar el estado de las URLs */
 const checkURL = async (url) => {
     try {
         const response = await axios.get(url)
         if(response.status === 200) {
             /** Notificacion verde si la respuesta es 200 OK */
-            notifier.notify({
+            const notificacionID = notifier.notify({
                 title: 'ARR Activo',
                 message: `El sistema en ${url} está respodiendo correctamente`,
                 icon: './iconos/success.svg',
-                
-                sound: true
+                sound: false,
+                appID: url
             })
+            notificacionMap.set(notificacionID, url);
         }
         
     } catch (error) {
-        notifier.notify({
+        const notificacionID = notifier.notify({
             title: 'ARR No Responde',
             message: `El sistema en ${url} no está respodiendo`,
             icon: './iconos/error.svg',
-            sound: true
+            sound: false,
+            appID: url
         })
+
+        notificacionMap.set(notificacionID, url);
     }
 }
 
-const abrirUrl = (url) => {
-    notifier.on('click', () => {
-        open(url)
-    })
-}
 
 /** Programar la verificacion cada minuto */
 schedule.scheduleJob('*/1 * * * *', () => {
     urls.forEach((url) => {
         checkURL(url);
-        abrirUrl(url)
     })
 })
 
 console.log('Iniciando monitorizacion de ARR cada minuto...');
+
