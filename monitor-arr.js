ac8d64d261c3b059aba610fa17ce30d406f6f62a
Eder <eder.arodriguez1999@gmail.com>
2024-09-19 01:13:49 -0600

    Done - Se quita el toast de EXITO y se agrega colores a los cosole log

--- monitor-arr.js
+++ monitor-arr.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import notifier from 'node-notifier';
 import schedule from 'node-schedule';
+import colors from 'colors';
 
 /** URLs a Monitorear */
 const urls = [
@@ -9,40 +10,33 @@ const urls = [
     'https://crm.bi.com.gt/test.asp'
 ]
 
-const notificacionMap = new Map();
 
 /** Funcion para verificar el estado de las URLs */
 const checkURL = async (url) => {
     try {
         const response = await axios.get(url)
         if(response.status === 200) {
-            /** Notificacion verde si la respuesta es 200 OK */
-            const notificacionID = notifier.notify({
-                title: 'ARR Activo',
-                message: `El sistema en ${url} está respodiendo correctamente`,
-                icon: './iconos/success.svg',
-                sound: false,
-                appID: url
-            })
-            notificacionMap.set(notificacionID, url);
+            console.log(`El sistema en ${url} está respodiendo correctamente`.green)
         }
         
     } catch (error) {
-        const notificacionID = notifier.notify({
+        notifier.notify({
             title: 'ARR No Responde',
             message: `El sistema en ${url} no está respodiendo`,
             icon: './iconos/error.svg',
             sound: false,
             appID: url
         })
-
-        notificacionMap.set(notificacionID, url);
+        console.log(`El sistema en ${url} no está respondiendo`.red);
+        
     }
 }
 
 
 /** Programar la verificacion cada minuto */
 schedule.scheduleJob('*/1 * * * *', () => {
+    console.clear()
+    console.log('URLs comprobados en el último minuto'.cyan);
     urls.forEach((url) => {
         checkURL(url);
     })
