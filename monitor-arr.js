e11a7ab12fbd588f4d948db5aa22f59e25d055d3
Eder <eder.arodriguez1999@gmail.com>
2024-09-19 04:38:26 -0600

    Se escribe en .csv cuando una url no responde

--- monitor-arr.js
+++ monitor-arr.js
@@ -3,24 +3,44 @@ import notifier from 'node-notifier';
 import schedule from 'node-schedule';
 import colors from 'colors';
 import moment from 'moment';
+import fs from 'fs';
+import fastCsv from 'fast-csv'
 
 /** URLs a Monitorear */
 const urls = [
     'https://www.bienlinea.bi.com.gt/test.asp',
-    'https://www.bibanking.bi.com.gt/test.asp',
-    'https://crm.bi.com.gt/test.asp'
+    'https://www.bibanking1.bi.com.gt/test.asp',
+    'https://crm1.bi.com.gt/test.asp'
 ]
 
+/** Archivo CSV */
+const csvFilePath = 'historico_fallos.csv';
+const fechaHora = moment().format('YYYY-MM-DD HH:mm:ss');
+
+/** Funcion para agregar un registro al CSV */
+const appendToCSV = (registro) => {
+        console.log(registro);
+        
+        const ws = fs.createWriteStream(csvFilePath, { flags: 'a'});
+    fastCsv
+        .write([registro], { headers: !fs.existsSync(csvFilePath)}) // Agrega el encabezado solo si el archivo no existe
+        .pipe(ws);
+} 
 
 /** Funcion para verificar el estado de las URLs */
 const checkURL = async (url) => {
     try {
-        const response = await axios.get(url)
+        const response = await axios(url)
         if(response.status === 200) {
             console.log(`El sistema en ${url} está respodiendo correctamente`.green)
         }
         
     } catch (error) {
+        const estado = error.response ? error.response.status : 'Sin respuesta';
+
+        /** Guardar en el CSV */
+        appendToCSV({ fechaHora, url, estado});
+
         notifier.notify({
             title: 'ARR No Responde',
             message: `El sistema en ${url} no está respodiendo`,
@@ -38,8 +58,7 @@ const checkURL = async (url) => {
 schedule.scheduleJob('*/1 * * * *', () => {
     console.clear()
 
-    const currentTime = moment().format('HH:mm');
-    console.log(`URLs comprados en el último minuto - Hora: ${currentTime}`.cyan);
+    console.log(`URLs comprados en el último minuto - FechaHora: ${fechaHora}`.cyan);
     urls.forEach((url) => {
         checkURL(url);
     })
